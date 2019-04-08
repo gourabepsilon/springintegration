8f1f40d7e10ba18c42ca80fafaa44454d105d5da
Gourab Guha <gourab.guha@ericsson.com>
2019-04-08 14:21:32 +0530

    Splitter moved

---
+++ Middleware/src/main/java/com/springboot/dummy/middleware/splitter/CustomerSplitter.java
@@ -0,0 +1,25 @@
+package com.springboot.dummy.middleware.splitter;
+
+import java.util.Arrays;
+
+import org.springframework.integration.splitter.AbstractMessageSplitter;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerSplitter extends AbstractMessageSplitter {
+
+	private final String TOKENIZER = ",";
+
+	public CustomerSplitter() {
+
+	}
+
+	@Override
+	protected Object splitMessage(Message<?> message) {
+		String msisdnList = message.getPayload().toString();
+		String[] msisdnArr = msisdnList.split(TOKENIZER);
+		return Arrays.asList(msisdnArr);
+	}
+
+}
